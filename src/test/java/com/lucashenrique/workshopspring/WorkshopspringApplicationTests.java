915e9b985e9912effd922faf1b8e771a09f865a5
Lucas Henrique <lhenrique5786@gmail.com>
2022-08-21 10:40:32 -0300

    Primeiro commit

---
+++ src/test/java/com/lucashenrique/workshopspring/WorkshopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucashenrique.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
